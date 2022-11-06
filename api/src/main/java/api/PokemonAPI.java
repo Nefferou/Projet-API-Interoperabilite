15f831cf85fdea09b5dd04c3c1372c4632c5361c
Nefferou <fertijulien@gmail.com>
2022-11-06 22:20:10 +0100

    Review

--- api/src/main/java/api/PokemonAPI.java
+++ api/src/main/java/api/PokemonAPI.java
@@ -2,14 +2,10 @@ package api;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pokemon.PokemonController;
